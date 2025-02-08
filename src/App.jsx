4804d98b59c1a3db61f8bbca1526f0c11dadd031
Abraao4raujo <68816619@gmail.com>
2025-02-08 15:00:10 -0300

    feat: adicionando tailwind no projeto

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,32 @@
 import React from "react";
 import { BrowserRouter, Outlet, Route, Routes } from "react-router-dom";
-import { GlobalStorage } from "./GlobalContext";
-
-import "./index.css";
 import Header from "../src/components/Header/Header";
 import Curriculo from "./components/Curriculo/Curriculo";
-import Tools from "./components/Tools/Tools";
 import MainPage from "./components/MainIndex/MainPage";
 import Templates from "./components/Templates/Templates";
 
 function Template() {
   return (
-    <>
+    <div className="w-screen h-full bg-gradient-to-br from-blue-50 to-purple-50">
       <Header />
       <Outlet />
-    </>
+    </div>
   );
 }
 
 const App = () => {
   return (
-    <div>
-      <React.StrictMode>
-        <BrowserRouter>
-          <Routes>
-            <Route element={<Template />}>
-              <Route path="/" element={<MainPage />} />
-              <Route path="/templates" element={<Templates />} />
-              <Route
-                path="/templates/curriculo"
-                element={
-                  <div className="pagina-curriculo">
-                    <GlobalStorage>
-                      <Tools />
-                      <Curriculo />
-                    </GlobalStorage>
-                  </div>
-                }
-              />
-            </Route>
-          </Routes>
-        </BrowserRouter>
-        {/* <RouterProvider router={router} /> */}
-      </React.StrictMode>
-    </div>
+    <React.StrictMode>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<Template />}>
+            <Route path="/" element={<MainPage />} />
+            <Route path="/templates" element={<Templates />} />
+            <Route path="/templates/curriculo" element={<Curriculo />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </React.StrictMode>
   );
 };
 
